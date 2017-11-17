fc44e5a4c964501f086fcec20bd3f5e0f2ee6634
louisc47 <louis.charbonier@ynov.com>
2017-11-17 15:05:34 +0100

    account

---
+++ src/event/event_history.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Moment from 'react-moment';
+import { Link, Redirect } from 'react-router-dom';
+
+class EventHistory extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: null,
+    }
+  }
+
+  componentDidMount(){
+    console.log(this.props.user);
+    axios.get('http://localhost:1337/user/'+this.props.user)
+    .then((response) => {
+      this.setState({user : response.data});
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  render() {
+    if(this.state.user){
+      return(
+          <div className="card">
+            <div className="card-body">
+              <img src="https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/256/User_man_male_profile_account_person_people.png" className="profilePic" />
+              {this.state.user.name}
+            </div>
+            <ul className="list-group list-group-flush">
+              {this.state.user.events.map((item) => {
+                return(
+                  <Link to={"/event/"+item.id}>
+                    <li className="list-group-item">
+                        <span className="badge">{item.status.name}</span>
+                      {item.name} le <Moment format="DD/MM/YYYY">{item.date}</Moment> à {item.place}
+                    </li>
+                  </Link>
+                );
+              })}
+            </ul>
+          </div>
+      );
+    }
+    else {
+      return (<p>chargement</p>)
+    }
+  }
+}
+
+export default EventHistory;
--- src/user/userDetails.js
+++ src/user/userDetails.js
@@ -6,84 +6,45 @@ import Moment from 'react-moment';
 import { Link, Redirect } from 'react-router-dom';
 import Opinion from '../opinion/opinion';
 import ListOpinion from '../opinion/list_opinion';
+import EventHistory from '../event/event_history';
 
 class userDetails extends Component {
   constructor(){
     super();
     this.state = {
-      user: null,
       actualise: 0,
     }
-  }
-
-  componentDidMount(){
-    this.getValue()
   }
 
   ChangeOpinion = (value) =>{
     this.setState({actualise : this.state.actualise + 1});
   }
 
-  getValue = () =>{
-    axios.get('http://localhost:1337/user/'+this.props.match.params.id)
-    .then((response) => {
-      this.setState({user : response.data});
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-
-  componentWillReceiveProps(){
-    this.getValue();
-  }
-
   render() {
-    if(this.state.user){
-      return(
-        <div>
-          <Menu />
-          <div className="col-sm-8 col-sm-offset-2">
-            <div className="card">
-              <div className="card-body">
-                <img src="https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/256/User_man_male_profile_account_person_people.png" className="profilePic" />
-                {this.state.user.name}
-              </div>
-              <ul className="list-group list-group-flush">
-                {this.state.user.events.map((item) => {
-                  return(
-                    <Link to={"/event/"+item.id}>
-                      <li className="list-group-item">
-                          <span className="badge">{item.status.name}</span>
-                        {item.name} le <Moment format="DD/MM/YYYY">{item.date}</Moment> à {item.place}
-                      </li>
-                    </Link>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
+    return(
+      <div>
+        <Menu />
 
-          <div className="col-sm-8 col-sm-offset-2 mt-5">
-            <div className="card">
-              <div className="card-body">
-                <Opinion recipient={this.props.match.params.id}
-                onChangeOpinion={this.ChangeOpinion}/>
-              </div>
-              <ul className="list-group list-group-flush">
-                <ListOpinion
-                  recipient={this.props.match.params.id}
-                  actualise={this.state.actualise}
-                />
-              </ul>
+        <div className="col-sm-8 col-sm-offset-2">
+          <EventHistory user={this.props.match.params.id}/>
+        </div>
+
+        <div className="col-sm-8 col-sm-offset-2 mt-5">
+          <div className="card">
+            <div className="card-body">
+              <Opinion recipient={this.props.match.params.id}
+              onChangeOpinion={this.ChangeOpinion}/>
             </div>
+            <ul className="list-group list-group-flush">
+              <ListOpinion
+                recipient={this.props.match.params.id}
+                actualise={this.state.actualise}
+              />
+            </ul>
           </div>
         </div>
-      );
-    }
-    else {
-      return (<p>chargement</p>)
-    }
+      </div>
+    );
   }
 }
 
