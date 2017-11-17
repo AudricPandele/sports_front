89f9622d2b31db9cff513316f3dc2797c073252c
AudricPandele <audric.pandele@ingesup.com>
2017-11-17 09:55:02 +0100

    Gestion erreur

--- src/account/Account.js
+++ src/account/Account.js
@@ -19,7 +19,7 @@ class Account extends Component {
     axios.get('http://localhost:1337/user/'+user_id)
     .then((response) => {
       this.setState({
-        data: response.data.user
+        data: response.data
       })
     })
     .catch(function (error) {
@@ -32,42 +32,47 @@ class Account extends Component {
       <div>
         <Menu active="account"/>
 
-        <div className="row">
-          <div className="col-sm-6 col-sm-offset-3">
-            <div className="card text-center">
-              <div className="card-header">
-                Account information
-              </div>
-              <div className="card-block">
-                <div className="row">
-                  <div className="col-sm-12">
-                    <h3 className="card-title">{this.state.data.name}</h3><br/>
-                  </div>
+        {!this.state.data ? (
+          <p>Loading</p>
+        ) : (
+          <div className="row">
+            <div className="col-sm-6 col-sm-offset-3">
+              <div className="card text-center">
+                <div className="card-header">
+                  Account information
                 </div>
-                <div className="row">
-                  <div className="text-left col-sm-4 col-sm-offset-4">
-                    <label>Email : {this.state.data.email}</label><br/>
-                    <label>Gender : {this.state.data.gender}</label><br/>
-                    <label>Birthday : {this.state.data.birthday}</label><br/>
+                <div className="card-block">
+                  <div className="row">
+                    <div className="col-sm-12">
+                      <h3>{this.state.data.name}</h3><br/>
+                    </div>
                   </div>
-                </div>
-                <a href="#" className="btn btn-primary">Save changes</a>
-                <div className="row">
-                  <div className="col-sm-12" style={{marginBottom: '10px'}}>
-                    <h3 className="card-title">My sports</h3><br/>
-                    {!this.state.data.sportList ? (
-                      <div>
-                        Chargement
-                      </div>
-                    ) : (
-                      <SportsList data={this.state.data.sportList}/>
-                    )}
+                  <div className="row">
+                    <div className="text-left col-sm-4 col-sm-offset-4">
+                      <label>Email : {this.state.data.email}</label><br/>
+                      <label>Gender : {this.state.data.gender}</label><br/>
+                      <label>Birthday : {this.state.data.birthday}</label><br/>
+                    </div>
+                  </div>
+                  <a href="#" className="btn btn-primary">Save changes</a>
+                  <div className="row">
+                    <div className="col-sm-12" style={{marginBottom: '10px'}}>
+                      <h3>My sports</h3><br/>
+                      {!this.state.data.sportList ? (
+                        <div>
+                          Chargement
+                        </div>
+                      ) : (
+                        <SportsList data={this.state.data.sportList}/>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
+
       </div>
 
     );
